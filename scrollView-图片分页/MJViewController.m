790486467afafefc23143cb005adfd15fa943c27
朱正晶 <bruce@zhuzhengjingdeMacBook-Pro.local>
2015-02-15 08:52:51 +0800

    scrollView分页功能

--- scrollView-图片分页/MJViewController.m
+++ scrollView-图片分页/MJViewController.m
@@ -8,7 +8,10 @@
 
 #import "MJViewController.h"
 
+#define kImageCount 8
+
 @interface MJViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -17,13 +20,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    CGFloat w = self.view.frame.size.width;
+    CGFloat h = self.view.frame.size.height;
+    
+    for (int i = 0; i < kImageCount; i++) {
+        // 1. 加载图片
+        UIImageView * imageView = [[UIImageView alloc] init];
+        NSString *imageName = [NSString stringWithFormat:@"0%d.jpg", i+1];
+        imageView.image = [UIImage imageNamed:imageName];
+        imageView.frame = CGRectMake(i * w, 0, w, h);
+        
+        [_scrollView addSubview:imageView];
+    }
+    
+    _scrollView.contentSize = CGSizeMake(kImageCount*w, 0);
+    _scrollView.showsHorizontalScrollIndicator = NO;
+    _scrollView.pagingEnabled = YES;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
