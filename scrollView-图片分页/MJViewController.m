6bc9cffb5f20e97a422d9afd5b608f013d83bd5b
朱正晶 <bruce@promote.cache-dns.local>
2015-03-04 12:51:24 +0800

    完成最新版

--- scrollView-图片分页/MJViewController.m
+++ scrollView-图片分页/MJViewController.m
@@ -13,6 +13,7 @@
 @interface MJViewController () <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property UIPageControl *pageControl;
+@property BOOL displayFlag;
 @end
 
 @implementation MJViewController
@@ -23,17 +24,36 @@
 	
     CGFloat w = self.view.frame.size.width;
     CGFloat h = self.view.frame.size.height;
+
+    // 1. 加载两张图片
+    UIImageView * imageView = [[UIImageView alloc] init];
+    imageView.image = [UIImage imageNamed:@"01.jpg"];
+    imageView.frame = CGRectMake(0, 0, w, h);
+    [_scrollView addSubview:imageView];
     
-    for (int i = 0; i < kImageCount; i++) {
-        // 1. 加载图片
-        UIImageView * imageView = [[UIImageView alloc] init];
-        NSString *imageName = [NSString stringWithFormat:@"0%d.jpg", i+1];
-        imageView.image = [UIImage imageNamed:imageName];
-        imageView.frame = CGRectMake(i * w, 0, w, h);
-        
-        [_scrollView addSubview:imageView];
-    }
+    /*
+    imageView = [[UIImageView alloc] init];
+    imageView.image = [UIImage imageNamed:@"02.jpg"];
+    imageView.frame = CGRectMake(320, 0, w, h);
     
+    [_scrollView addSubview:imageView];
+    */
+    NSLog(@"count = %d", _scrollView.subviews.count);
+    NSLog(@"subviews = %@", _scrollView.subviews);
+    
+    [_scrollView.subviews[0] removeFromSuperview];
+    [_scrollView.subviews[0] removeFromSuperview];
+    
+    NSLog(@"count = %d", _scrollView.subviews.count);
+    NSLog(@"subviews = %@", _scrollView.subviews);
+
+
+    /*
+    [_scrollView.subviews[2] removeFromSuperview];
+    
+    NSLog(@"count = %d", _scrollView.subviews.count);
+    NSLog(@"subviews = %@", _scrollView.subviews);
+    */
     _scrollView.contentSize = CGSizeMake(kImageCount*w, 0);
     _scrollView.showsHorizontalScrollIndicator = NO;
     _scrollView.pagingEnabled = YES;
@@ -48,12 +68,59 @@
     
     _pageControl = pageControl;
     [self.view addSubview:pageControl];
+    
+    _displayFlag = NO;
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
-    int page = scrollView.contentOffset.x / scrollView.frame.size.width;
+    CGFloat w = self.view.frame.size.width;
+    CGFloat h = self.view.frame.size.height;
+    CGFloat x = scrollView.contentOffset.x;
+    NSLog(@"x = %f", x);
+    
+    // 显示pageControl页面
+    int page = x / scrollView.frame.size.width;
     _pageControl.currentPage = page;
+    CGFloat left = x - scrollView.frame.size.width * page;
+    
+    NSLog(@"page = %d, left = %f", page, left);
+    
+    if ( left > 0.0 && _displayFlag == NO) {
+        //[_scrollView.subviews[3] removeFromSuperview];
+        NSLog(@"subviews0 = %@", _scrollView.subviews);
+        UIImageView * imageView = [[UIImageView alloc] init];
+        NSString *imageName = [NSString stringWithFormat:@"0%d.jpg", page + 2];
+        imageView.image = [UIImage imageNamed:imageName];
+        imageView.frame = CGRectMake((page + 1) * w, 0, w, h);
+        [_scrollView addSubview:imageView];
+        _displayFlag = YES;
+        NSLog(@"subviews1 = %@", _scrollView.subviews);
+    }
+    
+    if (left == 0.0 && page % 2 == 1) {
+        NSLog(@"subviews2 = %@", _scrollView.subviews);
+        _displayFlag = NO;
+        [_scrollView.subviews[0] removeFromSuperview];
+        
+        NSLog(@"subviews3 = %@", _scrollView.subviews);
+    } else if (left == 0.0 && page % 2 == 0) {
+        NSLog(@"subviews4 = %@", _scrollView.subviews);
+        _displayFlag = NO;
+        [_scrollView.subviews[3] removeFromSuperview];
+        NSLog(@"subviews = %@", _scrollView.subviews);
+    }
+    
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    NSLog(@"scrollViewDidEndDragging");
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView
+{
+    NSLog(@"scrollViewDidEndScrollingAnimation");
 }
 
 @end
